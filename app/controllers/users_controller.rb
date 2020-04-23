91e804f9921b22eb4515925c82dbcf699cd09d64
silverskg <chokichoki_gm@i.softbank.jp>
2020-04-23 21:38:56 +0900

    indexアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+  
   def new
     @user = User.new
   end
@@ -6,6 +10,7 @@ class UsersController < ApplicationController
   def create
     User.create(user_params)
   end
+
 
   private
 
