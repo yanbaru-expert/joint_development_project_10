26058350ee8f2aa7f53f08b68736162c301d7161
青木亮隆 <yori-goreng@aokiryoutakashinoMacBook-puro.local>
2020-04-25 14:05:14 +0900

    コマンドで「テーブル」と「モデル」を作成し，マイグレーションを行う

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_115256) do
+ActiveRecord::Schema.define(version: 2020_04_25_050222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "messages", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
