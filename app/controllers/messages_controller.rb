011914e0b2c7489b51bc776abfe660385cb46a7e
青木亮隆 <yori-goreng@aokiryoutakashinoMacBook-puro.local>
2020-04-25 16:10:10 +0900

    規投稿機能 new, create を実装（ストロングパラメータを利用）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,15 @@
 class MessagesController < ApplicationController
-    
+    def new
+        @message = Message.new
+    end
+
+    def create
+        Message.create(message_params)
+    end
+
+    private
+    def message_params
+        params.require(:message).permit(:name, :age)
+    end
+
 end
