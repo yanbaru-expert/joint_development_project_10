9ea30eb3886bd26da5a7d5d33044378ff22fc9ef
青木亮隆 <yori-goreng@aokiryoutakashinoMacBook-puro.local>
2020-04-25 14:22:50 +0900

    編集機能 edit, update を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,17 @@ class MessagesController < ApplicationController
         @message = Message.new
     end
 
+    def edit
+        @message= Message.find(params[:id])
+      end
+
     def show
         @message= Message.find(params[:id])
       end
+
+    def update
+        message = Message.find(params[:id])
+        message.update(message_params)
 
     def create
         Message.create(messge_params)
