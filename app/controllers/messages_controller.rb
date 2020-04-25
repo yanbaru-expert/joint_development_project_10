6902a2a2a07c490e62daecfc16aa0a709c317d05
青木亮隆 <yori-goreng@aokiryoutakashinoMacBook-puro.local>
2020-04-25 14:11:10 +0900

    新規投稿機能 new, create を実装（ストロングパラメータを利用）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,14 @@ class MessagesController < ApplicationController
     def new
         @message = Message.new
     end
+
+    def create
+        Message.create(messge_params)
+      end
+
+      private
+      def message_params
+        params.require(:message).permit(:name, :age)
+      end
+
 end
