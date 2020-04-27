0f7ce18e0a3ac85c7ae204cd5d2d0dba0e2a9110
kasyu-create <63037931+kasyu-create@users.noreply.github.com>
2020-04-27 20:21:27 +0900

    編集機能 edit, update を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,15 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+  end
 
   private
     def task_params
